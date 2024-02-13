ba6b152606034cc036952580a265c1314f9005f7
jmoo2880 <jmoo2880@uni.sydney.edu.au>
2024-02-14 05:45:35 +1100

    Add new cml7 dataset and benchmarking tables.

--- tests/test_SPIs.py
+++ tests/test_SPIs.py
@@ -9,87 +9,101 @@ import warnings
 
 ############# Fixtures and helper functions #########
 
-def load_benchmark_calcs():
-    benchmark_calcs = dict()
-    calcs = ['calc_standard_normal.pkl'] # follow this naming convention -> calc_{name}.pkl
-    for calc in calcs:
-            # extract the calculator name from the filename
-            calc_name = calc[len("calc_"):-len(".pkl")]
-
-            # Load the calculator
-            with open(f"tests/{calc}", "rb") as f:
-                loaded_calc = dill.load(f)
-                benchmark_calcs[calc_name] = loaded_calc
+def load_benchmark_tables():
+    """Function to load the mean and standard deviation tables for each MPI."""
+    table_fname = 'CML7_benchmark_tables.pkl'
+    with open(f"tests/{table_fname}", "rb") as f:
+        loaded_tables = dill.load(f)
     
-    return benchmark_calcs
+    return loaded_tables
 
-def load_benchmark_datasets():
-    benchmark_datasets = dict()
-    dataset_names = ['standard_normal.npy']
-    for dname in dataset_names:
-        dataset = np.load(f"pyspi/data/{dname}")
-        dataset = dataset.T 
-        benchmark_datasets[dname.strip('.npy')] = dataset
-
-    return benchmark_datasets
+def load_benchmark_dataset():
+    dataset_fname = 'cml7.npy'
+    dataset = np.load(f"pyspi/data/{dataset_fname}").T
+    return dataset
 
 def compute_new_tables():
     """Compute new tables using the same benchmark dataset(s)."""
-    benchmark_datasets = load_benchmark_datasets()
-    # Compute new tables on the benchmark datasets
-    new_calcs = dict()
+    benchmark_dataset = load_benchmark_dataset()
+    # Compute new tables on the benchmark dataset
+    np.random.seed(42)
+    calc = Calculator(dataset=benchmark_dataset)
+    calc.compute()
+    table_dict = dict()
+    for spi in calc.spis:
+        table_dict[spi] = calc.table[spi]
 
-    calc_base = Calculator() # create base calculator object
-
-    for dataset in benchmark_datasets.keys():
-        calc = deepcopy(calc_base) # make a copy of the base calculator
-        calc.load_dataset(dataset=benchmark_datasets[dataset])
-        calc.compute()
-        new_calcs[dataset] = calc
-
-    return new_calcs
+    return table_dict
 
 def generate_SPI_test_params():
-    """Generate combinations of calculator, dataset and SPI for the fixture."""
-    benchmark_calcs = load_benchmark_calcs()
-    new_calcs = compute_new_tables()
+    """Function to generate combinations of benchmark table, 
+    new table for each MPI"""
+    benchmark_tables = load_benchmark_tables()
+    new_tables = compute_new_tables()
     params = []
-    for calc_name, benchmark_calc in benchmark_calcs.items():
-        spi_dict = benchmark_calc.spis
-        for spi_est in spi_dict.keys():
-            params.append((calc_name, spi_est, benchmark_calc.table[spi_est], new_calcs[calc_name].table[spi_est]))
+    calc = Calculator()
+    spis = list(calc.spis.keys())
+    spi_ob = list(calc.spis.values())
+    for spi_est, spi_ob in zip(spis, spi_ob):
+        params.append((spi_est, spi_ob, benchmark_tables[spi_est], new_tables[spi_est].to_numpy()))
     
     return params
 
 params = generate_SPI_test_params()
 def pytest_generate_tests(metafunc):
     """Create a hook to generate parameter combinations for parameterised test"""
-    if "calc_name" in metafunc.fixturenames:
-        metafunc.parametrize("calc_name,est,mpi_benchmark,mpi_new", params)
+    if "est" in metafunc.fixturenames:
+        metafunc.parametrize("est, est_ob, mpi_benchmark,mpi_new", params)
+        
 
-def test_mpi(calc_name, est, mpi_benchmark, mpi_new):
+def test_mpi(est, est_ob, mpi_benchmark, mpi_new, spi_warning_logger):
     """Run the benchmarking tests."""
-
-    """First check to see if any SPIs are 'broken', as would be the case if
-     the benchmark table contains values for certain SPIs whereas the new table for the same
-      SPI does not (NaN). Also, if all values are NaNs for one SPI and not for the same SPI in the
-       newly computed table. """
+    zscore_threshold = 2 # 2 sigma
     
-    mismatched_nans = (mpi_benchmark.isna() != mpi_new.isna())
-    assert not mismatched_nans.any().any(), f"SPI: {est} | Dataset: {calc_name}. Mismatched NaNs."
+    # separate the the mean and std. dev tables for the benchmark
+    mean_table = mpi_benchmark['mean']
+    std_table = mpi_benchmark['std']
+
+    # check std stable for zeros and impute with smallest non-zero value
+    min_nonzero_std = np.nanmin(std_table[std_table > 0])
+    std_table[std_table == 0] = min_nonzero_std
+     
 
     # check that the shapes are equal
-    assert mpi_benchmark.shape == mpi_new.shape, f"SPI: {est}| Dataset: {calc_name}. Different table shapes. "
+    assert mean_table.shape == mpi_new.shape, f"SPI: {est}| Different table shapes. "
 
-    # Now quantify the difference between tables (if a diff exists)
-    epsilon = np.finfo(float).eps
+    # convert NaNs to zeros before proeceeding - this will take care of diagonal and any null outputs
+    mpi_new = np.nan_to_num(mpi_new)
+    mpi_mean = np.nan_to_num(mean_table)
 
-    if not mpi_benchmark.equals(mpi_new):
-        diff = abs(mpi_benchmark - mpi_new)
-        max_diff = diff.max().max()
-        if max_diff > epsilon:
-            warnings.warn(f"SPI: {est} | Dataset: {calc_name} | Max difference: {max_diff}")
+    # check if matrix is symmetric (undirected SPI) for num exceed correction
+    isSymmetric = "undirected" in est_ob.labels 
+
+    # get the module name for easy reference
+    module_name = est_ob.__module__.split(".")[-1]
+
+    if (mpi_new == mpi_mean).all() == False:
+        # tables are not equivalent, quantify the difference by z-scoring.
+        diff = abs(mpi_new - mpi_mean)
+        zscores = diff/std_table
+        idxs_greater_than_thresh = np.argwhere(zscores > zscore_threshold)
+        if len(idxs_greater_than_thresh) > 0:
+            sigs = list()
+            for idx in idxs_greater_than_thresh:
+                sigs.append(zscores[idx[0], idx[1]])
+            # get the max
+            max_z = max(sigs)
+            # number of interactions
+            num_iteractions = (mpi_new.shape[0] * mpi_new.shape[1]) - mpi_new.shape[0]
+            # count exceedances
+            num_exceed = len(sigs)
+            if isSymmetric:
+                # number of unique exceedences is half
+                num_exceed /= 2
+                num_iteractions /= 2
+
+            spi_warning_logger(est, module_name, max_z, int(num_exceed), int(num_iteractions))
+
     
     
-
 
