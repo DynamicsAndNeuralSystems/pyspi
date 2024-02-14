409208a33ce6c550be876a5550821be2cf2180dd
jmoo2880 <jmoo2880@uni.sydney.edu.au>
2024-02-14 11:57:44 +1100

    Bump to version 0.4.2 in setup.py.

--- setup.py
+++ setup.py
@@ -62,7 +62,7 @@ setup(
                         'data/standard_normal.npy',
                         'data/cml7.npy']},
     include_package_data=True,
-    version='0.4.1',
+    version='0.4.2',
     description='Library for pairwise analysis of time series data.',
     author='Oliver M. Cliff',
     author_email='oliver.m.cliff@gmail.com',
