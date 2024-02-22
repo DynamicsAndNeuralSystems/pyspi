7cc0cd6f0d19478a0d5f04b727b5b1d5b45d44ff
jmoo2880 <jmoo2880@uni.sydney.edu.au>
2024-02-22 15:08:44 +1100

    Added more filter spi unit tests.

--- tests/test_utils.py
+++ tests/test_utils.py
@@ -1,8 +1,15 @@
 from pyspi.utils import filter_spis
 import pytest
 
-def test_filter_spis_with_str_keyword():
+def test_filter_spis_invalid_keywords():
     """Pass in a dataype other than a list for the keywords"""
     with pytest.raises(TypeError) as excinfo:
         filter_spis("pyspi/config.yaml", "linear")
     assert "Keywords must be passed as a list" in str(excinfo.value), "Keywords must be passed as list error not shown."
+
+def test_filter_spis_with_invalid_config():
+    """Pass in an invalid/missing config file"""
+    with pytest.raises(FileNotFoundError):
+        filter_spis("invalid_config.yaml", ["test"])
+
+
